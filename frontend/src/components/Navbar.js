a42c6e6f60e22af3e6c79efe63d86f4d848ec454
tusharpamnani <tusharpamnani1@gmail.com>
2024-10-23 23:29:11 +0530

    incandescent-learning

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -20,8 +20,8 @@ function MainNavbar() {
               <Nav.Link >
                 <Link to='/Top50' style={{textDecoration:'none',color:'black'}}>Top50</Link>
               </Nav.Link>
-              <Nav.Link href="https://github.com/adavilalith/Book_Reccomendation_System" target='_blank'>Source Code</Nav.Link>
-              <Nav.Link href="https://www.linkedin.com/in/lalith-adavi-284378274" target='_blank'>Let's Connect!</Nav.Link>
+              <Nav.Link href="https://github.com/tusharpamnani/Book-Reccomender" target='_blank'>Source Code</Nav.Link>
+              <Nav.Link href="https://www.linkedin.com/in/tushar-pamnani" target='_blank'>Let's Connect!</Nav.Link>
           </Nav>  
         </Navbar.Collapse>
       </Container>
